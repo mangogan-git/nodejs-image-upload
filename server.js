53b7f20ce4134e50a38bf014f419a34b51f202f3
mango <mango@xsg.com.tw>
2020-08-28 17:49:45 +0800

    feat: return file via http Get

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: __dirname + '/uploads/images'});
+const path = require('path');
+const fs = require('fs');
+
+const DEST = 'uploads/';
+
+if (!fs.existsSync(DEST)) fs.mkdirSync(DEST);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, DEST);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+    }
+})
+
+const upload = multer({ storage: storage });
+
 
 const app = express();
 const PORT = 3000;
@@ -8,12 +25,30 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), (req, res) => {
-    if(req.file) {
+    if (req.file) {
         res.json(req.file);
     }
     else throw 'error';
 });
 
-app.listen(PORT, () => {
-    console.log('Listening at ' + PORT );
+
+app.get('/*', function (req, res) {
+    const ROOT_DIR = __dirname;
+
+    const filePath = path.join(ROOT_DIR, req.path);
+    fs.access(filePath, fs.constants.R_OK, (err) => {
+        if (err)
+            return notFound(req, res);
+        res.sendFile(filePath);
+    });
 });
+
+
+
+app.listen(PORT, () => {
+    console.log('Listening at ' + PORT);
+});
+
+function notFound(req, res, next) {
+    res.status(404).json({ error: 'endpoint/file not found' });
+}
